a3369ac15b505d900b08e7ae2056bb75c5125ca4
Fernando Karnagi <fkarnagi@gmail.com>
2022-08-14 09:34:52 +0800

    Initial Commit

---
+++ MySwiftApp/MySwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftAppApp.swift
+//  MySwiftApp
+//
+//  Created by Fernando Karnagi on 14/8/22.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
